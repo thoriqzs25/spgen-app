63d599150c3cea9f94a077f794e4c332a43e393c
Thariq Zhafran Satyagraha <71017985+thoriqzs25@users.noreply.github.com>
2023-02-13 00:24:39 +0700

    feat: add playlist, delete, etc

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Text } from '@chakra-ui/react';
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
 import SpotifyWebApi from 'spotify-web-api-js';
 import { urlLogin } from '../src/auth';
@@ -10,6 +10,8 @@ const Home = () => {
   const [userPl, setUserPl] = useState({
     items: [],
   });
+
+  const [inputPl, setInputPl] = useState('');
 
   const spotify = new SpotifyWebApi();
 
@@ -67,6 +69,25 @@ const Home = () => {
     });
   };
 
+  const addToPlaylist = (plId, tUri) => {
+    spotify.addTracksToPlaylist(plId, tUri).then(() => {
+      console.log('Success adding track to playlist');
+      getPlaylist(user.id);
+    });
+  };
+
+  const newPlaylist = () => {
+    spotify
+      .createPlaylist(user.id, { name: inputPl })
+      .then(() => {
+        console.log('Success creating new playlist');
+        getPlaylist(user.id);
+      })
+      .catch((e) => {
+        console.log('ERROR WHILE CREATING PLAYLIST', e);
+      });
+  };
+
   const Profile = () => {
     return (
       <Box marginBottom={'12px'}>
@@ -99,25 +120,52 @@ const Home = () => {
 
   const PlaylistCard = ({ item }) => {
     return (
-      <Flex marginBottom={'8px'}>
-        <Box bgColor={'redYoung'} width={'80px'} height={'80px'} marginRight={'8px'}>
-          {item.images.length > 0 ? (
-            <img src={item.images[0].url} style={{ objectFit: 'cover', width: 80, height: 80 }} />
-          ) : (
-            <Text>No Pict</Text>
-          )}
-        </Box>
-        <Box>
-          <Text>{item.name}</Text>
-        </Box>
-        <Button
-          justifySelf={'flex-end'}
-          onClick={() => deletePlaylist(item.id)}
-          cursor={'pointer'}
-          borderRadius={'4px'}
-          border={'1px solid black'}>
-          Delete
-        </Button>
+      <Flex marginBottom={'8px'} justifyContent={'space-between'} alignItems={'center'}>
+        <Flex>
+          <Box bgColor={'redYoung'} width={'80px'} height={'80px'} marginRight={'8px'}>
+            {item.images.length > 0 ? (
+              <img src={item.images[0].url} style={{ objectFit: 'cover', width: 80, height: 80 }} />
+            ) : (
+              <Text>No Pict</Text>
+            )}
+          </Box>
+          <Flex flexDir={'column'} justifyContent={'space-between'}>
+            <Text noOfLines={1}>{item.name}</Text>
+            <Text>Total track: {item.tracks.total}</Text>
+          </Flex>
+        </Flex>
+        <Flex flexDir={'column'} h={'full'}>
+          <Button
+            justifySelf={'flex-end'}
+            onClick={() => deletePlaylist(item.id)}
+            cursor={'pointer'}
+            borderRadius={'4px'}
+            border={'1px solid white'}
+            bgColor={'red'}
+            color={'white'}
+            p={'4px'}
+            h={'24px'}
+            fontSize={'12px'}
+            marginBottom={'4px'}>
+            <Text>Delete</Text>
+          </Button>
+          <Button
+            justifySelf={'flex-end'}
+            onClick={() => {
+              if (curr.item?.uri) addToPlaylist(item.id, [curr.item.uri]);
+              else checkTrack();
+            }}
+            cursor={'pointer'}
+            borderRadius={'4px'}
+            border={'1px solid white'}
+            bgColor={'greenYoung'}
+            color={'white'}
+            p={'4px'}
+            h={'24px'}
+            fontSize={'12px'}>
+            <Text>Add</Text>
+          </Button>
+        </Flex>
       </Flex>
     );
   };
@@ -134,17 +182,36 @@ const Home = () => {
         alignItems={'center'}>
         <Profile />
         {curr && (
-          <Flex flexDir={'column'} alignItems={'center'} marginBottom={'12px'} bgColor={'background'} w={'full'}>
+          <Flex
+            flexDir={'column'}
+            alignItems={'center'}
+            marginBottom={'12px'}
+            bgColor={'background'}
+            w={'full'}
+            paddingBottom={'8px'}>
             <Text>{curr.item.name}</Text>
             <img src={curr.item.album.images[1].url} width={80} height={80} />
           </Flex>
         )}
         {userPl.total > 0 && (
           <Box>
-            <Text fontSize={'20px'} fontWeight={'bold'}>
-              List Playlist
-            </Text>
-            <Box maxH={'400px'} bgColor={'yellowYoung'} overflowY={'scroll'} paddingTop={'8px'}>
+            <Flex marginBottom={'8px'}>
+              <Text fontSize={'20px'} fontWeight={'bold'} marginRight={'20px'}>
+                List Playlist
+              </Text>
+              <Input
+                type={'text'}
+                placeholder={'Nama playlist...'}
+                onChange={(e) => {
+                  setInputPl(e.target.value);
+                }}
+                w={'140px'}
+              />
+              <Button border={'1px solid black'} borderRadius={'8px'} onClick={() => newPlaylist()}>
+                +
+              </Button>
+            </Flex>
+            <Box w={'320px'} maxH={'400px'} bgColor={'yellowYoung'} overflowY={'scroll'} paddingTop={'8px'}>
               {userPl.items.map((pl, idx) => {
                 return <PlaylistCard item={pl} key={idx.toString()} />;
               })}
